70feb851f05630657c7201306054a90afcfaab3b
chaddymac <moodychadiamond@yahoo.com>
2021-09-24 00:21:35 -0400

    adding table and script

--- scripts/script.py
+++ scripts/script.py
@@ -1,45 +1,45 @@
 import json
 import boto3
 import string
-from bs4 import BeautifulSoup
+import os
+import sys
 
-dynamodb = boto3.resource('dynamodb', region_name="us-east-1")
-table = dynamodb.Table('alexasolutionsquiz')
+dynamodb = boto3.resource("dynamodb", region_name="us-east-1")
+table = dynamodb.Table('alexa_solutions_table')
 
-test = open("test.json")
-test2 = open("test2.json")
-test3 = open("test3.json")
-test4 = open("test4.json")
-test5= open("test5.json")
-#reads the json document
+
+check = os.listdir()
+test = open("scripts/test.json")
+# reads the json document
 qs = json.load(test)
-#going into the nested list 
+# going into the nested list
 results = qs["results"]
 id_inc = 0
 
-#looping through the results list of dictionaries to separate the items needed to be put in dynamo 
-for result in results + results2 + results3 + results4 + results5:
+# looping through the results list of dictionaries to separate the items needed to be put in dynamo
+for result in results:
     id_inc = id_inc + 1
     answers = result["prompt"]["answers"]
-    len_ans= len(answers)
+    len_ans = len(answers)
     letters = list(string.ascii_lowercase[:len_ans])
-    options = dict(zip(letters,answers))
-    item ={
+    options = dict(zip(letters, answers))
+    question = result.get("question_plain")
+    if question is None:
+        question = result['prompt']["question"]
+    item = {
         'id': id_inc,
-        'question':(result["question_plain"]),
-        'options':options,
-        'correct':result["correct_response"]
+        'question': question,
+        'options': options,
+        'correct': result["correct_response"]
     }
-    #putting the item dictionary into my dynamo table
+    # putting the item dictionary into my dynamo table
     response = table.put_item(Item=item)
 
-#adds count to dynamo. count holds the number of questions in the table   
+# adds count to dynamo. count holds the number of questions in the table
 table.put_item(Item={
-    "id":0,
-"count":id_inc
+    "id": 0,
+    "count": id_inc
 }
 )
 
-
-
-
+print("success")
